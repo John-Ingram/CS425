61369c195e0fd86e34596247a9830c875bdf253e
John Ingram <j123j7@gmail.com>
2023-11-24 22:02:09 -0600

    Finish Julia Assignment

--- JuliaAssignment/Julia-Ingram.jl
+++ JuliaAssignment/Julia-Ingram.jl
@@ -7,7 +7,32 @@
 # Design:
 # This program is designed to read information about Students, Courses, and Course Registrations
 # from a file and then output the information in a formatted way.
+# Since Julia is not object oriented, I used structs to create the types for the data.
+# I then created functions to read the data from the file, parse the data into the types,
+# and then register the students and courses. 
+# I then print the students and courses with their registrations.
 
+
+# Create the types
+# Create the student type
+struct Student
+    id::String
+    name::String
+    courses::Array{String}
+end
+
+# Create the course type
+struct Course
+    id::String
+    name::String
+    students::Array{String}
+end
+
+# Create the registration type
+struct Registration
+    studentId::String
+    courseId::String
+end
 
 # Read in the data from the file, making sure to only read the file once
 function readData(dataPath)
@@ -28,108 +53,69 @@ function readData(dataPath)
     return students, courses, registrations
 end
 
-# Function to parse the data into a dictionary {id => part before first whitespace, Content => part after first whitespace}
+# Function to parse the data into it's type
 # takes in an array of strings
-function parseData(data)
-    # Create an empty dictionary
-    parsedData = Dict()
+function parseData(data, type)
+    # Create a new list of objects
+    objects = []
     # Loop through the data
     for line in data
-        # Split the line into the id and the content
-        id, content = split(line, limit=2)
-        # Remove leading and trailing whitespace from the id and content
+        # Split the line into the id and the name
+        id, name = split(line, limit=2)
+        # Remove leading and trailing whitespace from the id and name
         id = strip(id)
-        content = strip(content)
-        # Add the id and content to the dictionary
-        dictToAdd = Dict()
-        dictToAdd["id"] = id
-        dictToAdd["content"] = []
-        push!(dictToAdd["content"], content)
-        # if the id is not already in the dictionary, add it othewise add the content to the existing id
-        if !haskey(parsedData, id)
-            parsedData[id] = dictToAdd
+        name = strip(name)
+        # Create a new object
+        if type == "students"
+            object = Student(id, name, [])
+        elseif type == "courses"
+            object = Course(id, name, [])
+        elseif type == "registrations"
+            object = Registration(id, name)
         else
-            push!(parsedData[id]["content"], content)
+            println("Error: Invalid type")
+            return
         end
-        
+        # Add the object to the list of objects
+        push!(objects, object)
     end
-    # Return the dictionary
-    return parsedData
+    # Return the list of objects
+    return objects
 end
 
-# Function to take in a dictionary of Students, Courses, and Registrations and return a 
-# dictionary of Students with the courses they are registered for, and a dictionary of Courses
-# with the students registered for them
+# Function to take in a list of students, courses, and registrations 
+# and add the course registrations to the students and the student registrations to the courses
 function register(students, courses, registrations)
-    # Create an empty dictionary for the students with the courses they are registered for
-    # The dictionary will be {studentId => {id => studentId, name => studentName, courses => [courseId1, courseId2, ...]}}
-    studentsWithCourses = Dict()
-    # Create an empty dictionary for the courses with the students registered for them
-    # The dictionary will be {courseId => {id => courseId, name => courseName, students => [studentId1, studentId2, ...]}}
-    coursesWithStudents = Dict()
     # Loop through the registrations
     for registration in registrations
-        for courseToRegister in registration[2]["content"]
-            # get the student id and course id from the registration
-            studentId, courseId = registration[2]["id"], courseToRegister
-
-            # Create the strings for the student and course ids to be printed
-            studentIdString = students[studentId]["content"][1] * " (Student ID: " * studentId * ")"
-            courseIdString = courses[courseId]["content"][1] * " (Course ID: " * courseId * ")"
-
-            # Add the student to the studentsWithCourses dictionary if they are not already in it
-            if !haskey(studentsWithCourses, studentId)
-                # Create a dictionary for the student
-                student = Dict()
-                # Add the student id to the dictionary
-                student["id"] = studentId
-                # Add the student name to the dictionary
-                student["name"] = students[studentId]["content"][1]
-                # Add an empty array for the courses to the dictionary
-                student["courses"] = []
-                # Add the student to the studentsWithCourses dictionary
-                studentsWithCourses[studentId] = student
-            end
-            
-            # Add the course to the coursesWithStudents dictionary if it is not already in it
-            if !haskey(coursesWithStudents, courseId)
-                # Create a dictionary for the course
-                course = Dict()
-                # Add the course id to the dictionary
-                course["id"] = courseId
-                # Add the course name to the dictionary
-                course["name"] = courses[courseId]["content"][1]
-                # Add an empty array for the students to the dictionary
-                course["students"] = []
-                # Add the course to the coursesWithStudents dictionary
-                coursesWithStudents[courseId] = course
-            end
-
-            # Add the courseid to the student's courses array
-            push!(studentsWithCourses[studentId]["courses"], courseIdString)
-            # Add the studentid to the course's students array
-            push!(coursesWithStudents[courseId]["students"], studentIdString)
-        end
+        # Find the student and course in the lists of students and courses
+        student = students[findfirst(x -> x.id == registration.studentId, students)]
+        course = courses[findfirst(x -> x.id == registration.courseId, courses)]
+        # Create the course registration string and the student registration string
+        courseRegistration = course.name * " (Course ID: " * course.id * ")"
+        studentRegistration = student.name * " (Student ID: " * student.id * ")"
+        # Add the course registration to the student
+        push!(student.courses, courseRegistration)
+        # Add the student registration to the course
+        push!(course.students, studentRegistration)
     end
-    # Return the dictionaries
-    return studentsWithCourses, coursesWithStudents
 end
 
 students, courses, registrations = readData("register.txt")
-students = parseData(students)
-courses = parseData(courses)
-registrations = parseData(registrations)
+students = parseData(students, "students")
+courses = parseData(courses, "courses")
+registrations = parseData(registrations, "registrations")
 
-studentsWithCourses, coursesWithStudents = register(students, courses, registrations)
+register(students, courses, registrations)
 
 println("\nStudents:")
-for student in studentsWithCourses
-    println("\tStudent: " * student[2]["name"] * " (Student ID: " * student[2]["id"] * ")")
+for student in students
+    println("\tStudent: " * student.name * " (Student ID: " * student.id * ")")
     # Print the courses with the fist course on that line, and the rest indented
     print("\tCourses: ")
-    for course in student[2]["courses"]
+    for course in student.courses
         # Don't indent the first course
-        if course == student[2]["courses"][1]
+        if course == student.courses[1]
             print(course)
         else
             print("\n\t\t ", course)
@@ -139,13 +125,13 @@ for student in studentsWithCourses
 end
 
 println("\nCourses:")
-for course in coursesWithStudents
-    println("\tCourse: " * course[2]["name"] * " (Course ID: " * course[2]["id"] * ")")
+for course in courses
+    println("\tCourse: " * course.name * " (Course ID: " * course.id * ")")
     # Print the students with the fist student on that line, and the rest indented
     print("\tStudents: ")
-    for student in course[2]["students"]
+    for student in course.students
         # Don't indent the first student
-        if student == course[2]["students"][1]
+        if student == course.students[1]
             print(student)
         else
             print("\n\t\t  ", student)
@@ -153,3 +139,13 @@ for course in coursesWithStudents
     end
     println("\n")
 end
+
+# I enjoyed messing around with Julia. One thing that really shocked me was how intuitive the syntax was.
+# When learing a new language, I will often assume syntax, and then be wrong and look it up. But with Julia,
+# I was often right about the syntax. Having types instead of objects felt a little odd, but my code wound
+# up being very simple to writem and easy to read. I didn't implement the student and course counts, because
+# tallying those would have required a function to operate on each list. I realize that the total number of
+# function calls may have been similar to an object oriented solution, but I feel like the code would be 
+# far less clean. I definitely see the appeal of Julia for data science, but since I'm not a data scientist, 
+# I don't think I'll be using it much. I would use Julia before R or Matlab, but I would probably use 
+# Python before Julia, since I'm more familiar with Python.
